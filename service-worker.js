f576105cbb4f8e34c2f438fef03fcf5230c03abb
mohdishaqkhan <ishaqinbox@gmail.com>
2025-08-21 11:34:15 +0530

    Update service-worker.js
    gitcp
    
    Signed-off-by: mohdishaqkhan <ishaqinbox@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,11 @@
-var key = config.SECRET_API_KEY;
+// When user submits a prompt
+fetch('/api/data', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ prompt: userPrompt }) // userPrompt is what the user typed
+})
+.then(response => response.json())
+.then(data => {
+  // Handling api response here
+});
+
