7eaf9b7b3791a006ea05fffc6da3706583746f44
mohdishaqkhan <ishaqinbox@gmail.com>
2025-08-22 13:18:29 +0530

    Update service-worker.js
    Signed-off-by: mohdishaqkhan <ishaqinbox@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,12 @@
-// When user submits a prompt
-fetch('/api/data', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ prompt: userPrompt }) // userPrompt is what the user typed
-})
-.then(response => response.json())
-.then(data => {
-  // Handling api response here
-});
+// // When user submits a prompt
+// fetch('/api/data', {
+//   method: 'POST',
+//   headers: { 'Content-Type': 'application/json' },
+//   body: JSON.stringify({ prompt: userPrompt }) // userPrompt is what the user typed
+// })
+// .then(response => response.json())
+// .then(data => {
+//   // Handling api response here
+// });
+
 
